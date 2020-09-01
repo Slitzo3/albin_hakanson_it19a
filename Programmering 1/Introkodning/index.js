54d35ca794ce57f54573d24a5a786181834d62f1
Albin Håkanson <33166817+Tooxic@users.noreply.github.com>
2020-09-01 09:27:25 +0200

    Added files

---
+++ Programmering 1/Introkodning/index.js
@@ -0,0 +1,11 @@
+let name = "Albin"
+let xd = "xd"
+let age = "17"
+
+console.log(`Hej ${name}, Du är ${xd}`)
+
+if (age >= 18){
+    console.log("Du är vuxen")
+} else { 
+    console.log("DU ÄR ETT BARN")
+}
