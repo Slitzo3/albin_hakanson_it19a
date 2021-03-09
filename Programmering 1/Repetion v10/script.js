56846cf9031f7da66fc93c49b2e0c73a15881c4a
Albin Håkanson <33166817+Tooxic@users.noreply.github.com>
2021-03-09 09:06:37 +0100

    Idk 😎

---
+++ Programmering 1/Repetion v10/script.js
@@ -0,0 +1,17 @@
+console.log("Repetion v10")
+
+let namn = "Albin"
+let alder = 17
+
+console.log(`Hej ${namn}, Du är ${alder} år gammal`)
+
+if (alder > 18){
+    console.log("Du är vuxen")
+} else {
+    console.log("Du är barn");
+}
+
+for (let i = 0; i < 20; i++){
+    if (i % 2 == 0)
+    console.log(1);
+}
